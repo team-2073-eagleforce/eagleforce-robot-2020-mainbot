7a14f4d6c03661f1ed95f20816cc45d999a5d4ef
matthew <you@example.com>
2020-01-11 11:42:28 -0800

    IR-3:filled in OI, ApplicationContext, and AppConstants with buttons and controllers

---
+++ src/main/java/com/team2073/robot/AppConstants.java
@@ -0,0 +1,11 @@
+package com.team2073.robot;
+
+public abstract class AppConstants {
+    public abstract class Ports {
+
+        //DS
+        public static final int WHEEL_PORT = 0;
+        public static final int JOYSTICK_PORT = 1;
+        public static final int CONTROLLER_PORT = 2;
+    }
+}
--- src/main/java/com/team2073/robot/OperatorInterface.java
+++ src/main/java/com/team2073/robot/OperatorInterface.java
@@ -1,9 +1,45 @@
 package com.team2073.robot;
 
+import com.team2073.common.trigger.ControllerTriggerTrigger;
 import com.team2073.robot.ctx.ApplicationContext;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.POVButton;
 
 public class OperatorInterface {
 
     private static ApplicationContext appCtx = ApplicationContext.getInstance();
 
+    private Joystick controller = appCtx.getController();
+    private Joystick driveWheel = appCtx.getDriveWheel();
+    private Joystick driveStick = appCtx.getDriveStick();
+
+    private JoystickButton a = new JoystickButton(controller, 1);
+    private JoystickButton b = new JoystickButton(controller, 2);
+    private JoystickButton x = new JoystickButton(controller, 3);
+    private JoystickButton y = new JoystickButton(controller, 4);
+    private JoystickButton lb = new JoystickButton(controller, 5);
+    private JoystickButton rb = new JoystickButton(controller, 6);
+    private JoystickButton controllerBack = new JoystickButton(controller, 7);
+    private JoystickButton controllerStart = new JoystickButton(controller, 8);
+    private POVButton dPadUp = new POVButton(controller, 0);
+    private POVButton dPadRight = new POVButton(controller, 90);
+    private POVButton dPadDown = new POVButton(controller, 180);
+    private POVButton dPadLeft = new POVButton(controller, 270);
+    private JoystickButton backTrigger = new JoystickButton(driveStick, 1);
+    private JoystickButton stickTwo = new JoystickButton(driveStick, 2);
+    private JoystickButton stickThree = new JoystickButton(driveStick, 3);
+    private JoystickButton stickFour = new JoystickButton(driveStick, 4);
+    private JoystickButton stickFive = new JoystickButton(driveStick, 5);
+    private JoystickButton stickTen = new JoystickButton(driveStick, 10);
+
+    private JoystickButton leftPaddle = new JoystickButton(driveWheel, 1);
+    private JoystickButton wheelCircle = new JoystickButton(driveWheel, 2);
+    private JoystickButton rightPaddle = new JoystickButton(driveWheel, 3);
+    private JoystickButton wheelTriangle = new JoystickButton(driveWheel, 4);
+    private ControllerTriggerTrigger leftTrigger = new ControllerTriggerTrigger(controller, 2);
+    private ControllerTriggerTrigger rightTrigger = new ControllerTriggerTrigger(controller, 3);
+
+    private ControllerTriggerTrigger rightWheelButton = new ControllerTriggerTrigger(driveWheel, 3);
+    private ControllerTriggerTrigger leftWheelButton = new ControllerTriggerTrigger(driveWheel, 2);
 }
