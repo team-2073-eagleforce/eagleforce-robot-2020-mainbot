46339ce0291f75603d3920e2d95e5e05af82f154
SIsForSarcasm <gmckenna099@gmail.com>
2020-01-13 17:41:30 -0800

    IR-4: Added methods that track rotation count and stop motor after specified number of rotations ( rotation control ).

---
+++ src/main/java/com/team2073/robot/ctx/ApplicationContext.java
@@ -0,0 +1,53 @@
+package com.team2073.robot.ctx;
+
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Joystick;
+
+import static com.team2073.robot.AppConstants.Ports.*;
+
+public class ApplicationContext {
+
+    private static ApplicationContext instance;
+    private Joystick controller;
+    private Joystick driveWheel;
+    private Joystick driveStick;
+
+    private ColorSensorV3 colorSensor;
+
+    public static ApplicationContext getInstance() {
+        if (instance == null) {
+            instance = new ApplicationContext();
+        }
+        return instance;
+    }
+
+    public Joystick getController() {
+        if (controller == null) {
+            controller = new Joystick(CONTROLLER_PORT);
+        }
+        return controller;
+    }
+    public Joystick getDriveWheel() {
+        if (driveWheel == null){
+            driveWheel = new Joystick(WHEEL_PORT);
+        }
+        return driveWheel;
+    }
+
+    public Joystick getDriveStick() {
+        if (driveStick == null) {
+            driveStick = new Joystick(JOYSTICK_PORT);
+        }
+        return driveStick;
+    }
+
+    public ColorSensorV3 getColorSensor() {
+        if (colorSensor == null) {
+            colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+        }
+        return colorSensor;
+    }
+
+
+}
--- src/main/java/com/team2073/robot/subsystem/WOFManipulatorSubsystem.java
+++ src/main/java/com/team2073/robot/subsystem/WOFManipulatorSubsystem.java
@@ -7,28 +7,29 @@ import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 import com.team2073.common.periodic.PeriodicRunnable;
 import com.team2073.robot.ctx.ApplicationContext;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
 
 import java.util.ArrayList;
 
 public class WOFManipulatorSubsystem implements PeriodicRunnable {
+    private static ApplicationContext appCtx = ApplicationContext.getInstance();
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
     private final ColorMatch m_colorMatcher = new ColorMatch();
     private TalonSRX talonSRX = new TalonSRX(1);
-
-
-//    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    String gameData = DriverStation.getInstance().getGameSpecificMessage();
+    private Color kGreenTarget = ColorMatch.makeColor(88 / 255d, 170 / 255d, 67 / 255d);
+    private Color kRedTarget = ColorMatch.makeColor(113 / 255d, 65 / 255d, 27 / 255d);
+    private Color kYellowTarget = ColorMatch.makeColor(150 / 255d, 150 / 255d, 45 / 255d);
+    //    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
 //    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
 //    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
 //    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
     private Color kBlueTarget = ColorMatch.makeColor(60 / 255d, 136 / 255d, 107 / 255d);
-    private Color kGreenTarget = ColorMatch.makeColor(88 / 255d, 170 / 255d, 67 / 255d);
-    private Color kRedTarget = ColorMatch.makeColor(113 / 255d, 65 / 255d, 27 / 255d);
-    private Color kYellowTarget = ColorMatch.makeColor(150 / 255d, 150 / 255d, 45 / 255d);
-
-    private static ApplicationContext appCtx = ApplicationContext.getInstance();
+    private int counter = 0;
+    private int rotations = 0;
 
     @Override
     public void onPeriodic() {
@@ -109,13 +110,25 @@ public class WOFManipulatorSubsystem implements PeriodicRunnable {
         talonSRX.set(ControlMode.PercentOutput, output);
     }
 
+    private void rotationCounter(String colorName) {
+        setMotor(.2);
+        if (readColor().equals(colorName)) {
+            counter += 1;
+        }
+    }
+
+    private void stopOnRotation() {
+        if (counter >= 6) {
+            setMotor(0);
+        }
+    }
+
     private void stopOnColor(String colorName) {
         if (readColor().equals(colorName)) {
-            setMotor(20d);
+            setMotor(.2);
         } else {
             setMotor(0d);
+        }
     }
-}
-
 }
 
