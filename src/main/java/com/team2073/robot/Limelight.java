30ea8e5ae37f5a518604e2c363e0f653094a6be3
Gurvir11 <gurvirkooner11@gmail.com>
2020-01-27 19:35:03 -0800

    IR-11: Added Area Based Distance Calculation

--- src/main/java/com/team2073/robot/Limelight.java
+++ src/main/java/com/team2073/robot/Limelight.java
@@ -4,16 +4,16 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
 
-    private static final double LIMELIGHT_HIGH_HEIGHT = 39d;
+/*    private static final double LIMELIGHT_HIGH_HEIGHT = 42d;
     private static final double LIMELIGHT_LOW_HEIGHT = 23d;
-    private static final double LIMELIGHT_LENS_ANGLE = Math.toRadians(18d);
-    private static final double TARGET_HEIGHT = (8 * 12 + 2.25) - 15;
+    private static final double LIMELIGHT_LENS_ANGLE = Math.toRadians(16d);
+    private static final double TARGET_HEIGHT = (8 * 12 + 2.25) - 15;*/
 
-    public double getTy() {
+    public double getTx() {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
     }
 
-    public double getTx() {
+    public double getTy() {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
     }
 
@@ -29,12 +29,16 @@ public class Limelight {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tvert").getDouble(0);
     }
 
-    public double getHighDistance(){
+/*    public double getHighDistance(){
         return (TARGET_HEIGHT - LIMELIGHT_HIGH_HEIGHT) / (Math.tan(LIMELIGHT_LENS_ANGLE  + Math.toRadians(getTy())));
     }
 
     public double getLowDistance(){
         return (TARGET_HEIGHT - LIMELIGHT_LOW_HEIGHT) / (Math.tan(LIMELIGHT_LENS_ANGLE  + Math.toRadians(getTy())));
+    }*/
+
+    public double getAreaBasedDistance() {
+        return 303.31 * Math.pow(0.91, getTa());
     }
 
 //    public void changeConfig(Target target) {
