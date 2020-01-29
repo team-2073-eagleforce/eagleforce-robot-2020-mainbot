f1462dbbc8b8e1bc29efd16b2e860a35d6ccb408
Gurvir11 <gurvirkooner11@gmail.com>
2020-01-28 16:37:00 -0800

    IR-11: Redoing to do a fixed point calculation

--- src/main/java/com/team2073/robot/Limelight.java
+++ src/main/java/com/team2073/robot/Limelight.java
@@ -4,10 +4,10 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
 
-/*    private static final double LIMELIGHT_HIGH_HEIGHT = 42d;
+    private static final double LIMELIGHT_HIGH_HEIGHT = 42d;
     private static final double LIMELIGHT_LOW_HEIGHT = 23d;
-    private static final double LIMELIGHT_LENS_ANGLE = Math.toRadians(16d);
-    private static final double TARGET_HEIGHT = (8 * 12 + 2.25) - 15;*/
+    private static final double LIMELIGHT_LENS_ANGLE = Math.toRadians(13d);
+    private static final double TARGET_HEIGHT = (8 * 12 + 2.25) - 15;
 
     public double getTx() {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
@@ -29,13 +29,13 @@ public class Limelight {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tvert").getDouble(0);
     }
 
-/*    public double getHighDistance(){
+    public double getHighDistance(){
         return (TARGET_HEIGHT - LIMELIGHT_HIGH_HEIGHT) / (Math.tan(LIMELIGHT_LENS_ANGLE  + Math.toRadians(getTy())));
     }
 
     public double getLowDistance(){
         return (TARGET_HEIGHT - LIMELIGHT_LOW_HEIGHT) / (Math.tan(LIMELIGHT_LENS_ANGLE  + Math.toRadians(getTy())));
-    }*/
+    }
 
     public double getAreaBasedDistance() {
         return 303.31 * Math.pow(0.91, getTa());
