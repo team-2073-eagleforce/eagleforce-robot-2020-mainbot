6857ea3008cb3f001d66b00b6fef8660d5e52fdc
Jason2073 <stanleytechrepair@gmail.com>
2020-02-04 14:50:15 -0800

    fixing merge conflicts

--- src/main/java/com/team2073/robot/command/intake/IntakeToggleSolenoidsCommand.java
+++ src/main/java/com/team2073/robot/command/intake/IntakeToggleSolenoidsCommand.java
@@ -1,7 +1,7 @@
 package com.team2073.robot.command.intake;
 
 import com.team2073.common.command.AbstractLoggingCommand;
-import com.team2073.robot.ctx.ApplicationContext;
+import com.team2073.robot.ApplicationContext;
 import com.team2073.robot.subsystem.IntakeSubsystem;
 
 public class IntakeToggleSolenoidsCommand extends AbstractLoggingCommand {
